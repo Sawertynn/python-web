29d60ccfb91ae35a7692059d2aee686cc85de1d0
tkurzela <sawertynn@gmail.com>
2024-02-01 17:14:01 +0100

    complete overhaul: copy from tutorial repo

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,35 +1,51 @@
 import os
-from flask import Flask
 
-from flaskr import db, auth, blog
+from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the app
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
     else:
-        app.config.from_mapping(test_config)
+        # load the test config if passed in
+        app.config.update(test_config)
 
     # ensure the instance folder exists
-    os.makedirs(app.instance_path, exist_ok=True)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-    # a simple subpage that says hello
     @app.route("/hello")
     def hello():
-        return "Hello from subpage!"
+        return "Hello, World!"
+
+    # register the database commands
+    from . import db
 
     db.init_app(app)
 
-    app.register_blueprint(auth.bp)
+    # apply the blueprints to the app
+    from . import auth
+    from . import blog
 
+    app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
 
     return app
